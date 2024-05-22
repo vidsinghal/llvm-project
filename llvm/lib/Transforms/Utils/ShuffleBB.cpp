eeff0dbca8828137e4d34ebd69e4f9df7079a7e6
Vidush Singhal <singhal2@ruby966.llnl.gov>
2024-05-22 08:47:28 -0700

    saving work

--- llvm/lib/Transforms/Utils/ShuffleBB.cpp
+++ llvm/lib/Transforms/Utils/ShuffleBB.cpp
@@ -10,10 +10,7 @@
 
 using namespace llvm;
 
-
 int getNewIndex(std::vector<int> &IntVector);
-//Function::iterator getOriginalAtIndex(int index);
-//int decrementIndices(int currIndex, std::vector<int> &vec);
 
 Function::iterator ShuffleBasicBlocksPass::getOriginalAtIndex(int index) {
   int startIndex = 0;
@@ -26,68 +23,11 @@ Function::iterator ShuffleBasicBlocksPass::getOriginalAtIndex(int index) {
   }
 }
 
-// int decrementIndices(int currIndex, std::vector<int> &vec){
-
-//   for (int &index : vec){
-//     if (index >= currIndex){
-//       index = index - 1;
-//     }
-//   }
-
-// }
-
-
-// auto returnIteratorAtIndex(int currIndex, Function &F){
-
-//   int startIndex = 0;
-//   for (Function::iterator start = F.begin(), end = F.end(); start != end; start++){
-//     if (startIndex == currIndex){
-//       return start;
-//     }
-//     else{
-//       startIndex++;
-//     }
-//   }
-// }
-
 PreservedAnalyses ShuffleBasicBlocksPass::run(Function &F,
                                               FunctionAnalysisManager &AM) {
 
   
-  
-  // int numBB = F.size();
-  // auto FromIt = returnIteratorAtIndex(1, F);
-  // auto ToIt = returnIteratorAtIndex()
-  // splice(ToIt, F, FromIt);
-
-  // int i=0; 
-  // for (BasicBlock &B : BasicBlocks){
-    
-  //   //Cannot change order of the Entry Basic Block.
-    
-  //   if (i != 0)
-  //     BasicBlock *RemovedBlock = BasicBlocks.remove(B);
-  //   //TempBasicBlocksList.push_front(RemovedBlock);
-
-  //   i++;
-
-  // }
-
-  //reverse the order of the remaining blocks 
-  //for (BasicBlock &B : TempBasicBlocksList){
-
-  //  BasicBlock *RemovedBlock = TempBasicBlocksList.remove(B);
-  //  BasicBlocks.push_back(RemovedBlock);
-
-  //}
-
-  //generateRandomBasicBlockPermutation(NumBasicBlocks, TempBasicBlocksList,
-  //                                    BasicBlocks);
-
-  //for (const BasicBlock &BB : F){
-  //  BasicBlocks.push_back(&BB);
-  //}
-
+  srand(time(NULL));
   //preserve original basic blocks order. 
   int i = 0;
   for (Function::iterator start = F.begin(); start != F.end(); start++){
@@ -108,18 +48,6 @@ PreservedAnalyses ShuffleBasicBlocksPass::run(Function &F,
 
   BasicBlocks.clear();
   errs() << "\n";
-  
-
-  //auto SecondBlock = F.end();
-  //F.splice(F.end(), &F, F.end());
-
-  //std::copy(BasicBlocks.begin(), BasicBlocks.end(), std::ostream_iterator<const BasicBlock*>(std::cout, " "));
-  //std::cout << '\n';
-
-  //std::shuffle(BasicBlocks.begin(), BasicBlocks.end(), g);
-
-  //std::copy(BasicBlocks.begin(), BasicBlocks.end(), std::ostream_iterator<const BasicBlock*>(std::cout, " "));
-  //std::cout << '\n';
 
   return PreservedAnalyses::none();
 }
@@ -143,15 +71,13 @@ void ShuffleBasicBlocksPass::generateRandomBasicBlockPermutation(Function &F) {
     IntVector[i] = i;
   }
   
+  //always insert new blocks from the tail of the list. 
   Function::iterator InsertPoint = F.end();
-  //we don't want to insert before the entry block. 
-  //InsertPoint++;
   while (!IntVector.empty()) {
     int NewIndex = getNewIndex(IntVector);
     errs() << NewIndex << "\n";
 
     Function::iterator RemovedBasicBlock = getOriginalAtIndex(NewIndex); 
     F.splice(InsertPoint, &F, RemovedBasicBlock);
-    //InsertPoint = RemovedBasicBlock;
   }
 }
