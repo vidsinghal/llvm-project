d0c453dcf6907c668c450b23d2e1a7c7940611d7
vidush <vidush.sl@gmail.com>
2025-11-18 17:53:49 -0500

    Fix rebase error, pass AK to addAccess

--- llvm/lib/Transforms/IPO/AttributorAttributes.cpp
+++ llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -1533,8 +1533,8 @@ struct AAPointerInfoImpl
           if (!IsMustAcc)
             AK = AccessKind((AK & ~AK_MUST) | AK_MAY);
           Changed |=
-              addAccess(A, NewRanges, CB, RAcc.getContent(), RAcc.getKind(),
-                        RAcc.getType(), OffsetInfoMap, RAcc.getRemoteInst());
+              addAccess(A, NewRanges, CB, RAcc.getContent(), AK, RAcc.getType(),
+                        OffsetInfoMap, RAcc.getRemoteInst());
         }
       }
     }
